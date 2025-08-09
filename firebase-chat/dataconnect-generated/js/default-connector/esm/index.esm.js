f410e73faf4d83727634028cf849c857a1a35165
daniellesarusi <81098880+daniellesarusi@users.noreply.github.com>
2025-08-09 14:15:35 +0300

    Delete firebase-chat directory

--- firebase-chat/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'firebase-chat',
-  location: 'asia-east1'
-};
-
--- firebase-chat/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'firebase-chat',
-  location: 'asia-east1'
-};
-exports.connectorConfig = connectorConfig;
